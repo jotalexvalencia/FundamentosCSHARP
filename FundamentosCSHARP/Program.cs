f5c415ae79b142a13917ad42ee4498a874abc4d2
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-06-28 09:38:51 -0500

    Agregar archivos de proyecto.

---
+++ FundamentosCSHARP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace FundamentosCSHARP
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
