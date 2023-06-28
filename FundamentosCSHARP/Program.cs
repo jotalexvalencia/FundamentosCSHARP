8c11e3e998d5dec4c51bf713c3e3e6857969c231
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-06-28 10:33:53 -0500

    explicación de tipos de datos  y var

--- FundamentosCSHARP/Program.cs
+++ FundamentosCSHARP/Program.cs
@@ -5,7 +5,13 @@ namespace FundamentosCSHARP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            byte numero = 255; // 0 -255 no permite negativos
+            sbyte numero2 = -128; // 0 - 255 puedo poner números negativos se reduce desde -128 hasta 127
+            uint numero3 = 0; // positivos la u
+            byte? numero4 = null;//permite null
+
+            var persona = new { nombre = "Héctor", apellido = "De León", };
+            Console.WriteLine(persona.nombre);
         }
     }
 }
