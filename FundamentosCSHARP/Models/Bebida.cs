f2eb262722dae5ac85c0d8ddc870115be553c7fd
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-07-11 19:56:56 -0500

    tipos de datos y var Merge branch 'main' of https://github.com/jotalexvalencia/FundamentosCSHARP

---
+++ FundamentosCSHARP/Models/Bebida.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentosCSHARP.Models
+{
+    class Bebida
+    {
+        public Bebida(string Nombre, int Cantidad)
+        {
+            this.Nombre = Nombre;
+            this.Cantidad = Cantidad;
+        }
+
+        public string Nombre { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public void Beberse(int CuantoBebio) 
+        { 
+            this.Cantidad -= CuantoBebio;
+        }
+    }
+}
--- FundamentosCSHARP/Program.cs
+++ FundamentosCSHARP/Program.cs
@@ -1,16 +1,26 @@
-﻿namespace FundamentosCSHARP
+﻿using System;
+using FundamentosCSHARP.Models;
+namespace FundamentosCSHARP
 {
     class Program
     {
         static void Main(string[] args)
         {
-            byte numero = 255; // 0 -255 no permite negativos
-            sbyte numero2 = -128; // 0 - 255 puedo poner números negativos se reduce desde -128 hasta 127
-            uint numero3 = 0; // positivos la u
-            byte? numero4 = null;//permite null
+            //byte numero = 255; // 0 -255 no permite negativos
+            //sbyte numero2 = -128; // 0 - 255 puedo poner números negativos se reduce desde -128 hasta 127
+            //uint numero3 = 0; // positivos la u
+            //byte? numero4 = null;//permite null
 
-            var persona = new { nombre = "Héctor", apellido = "De León", };
-            Console.WriteLine(persona.nombre);
+            //var persona = new { nombre = "Héctor", apellido = "De León", };
+            //Console.WriteLine(persona.nombre);
+
+            // 2 clases objetos y constructores
+            Bebida bebida = new Bebida("Coca cola", 1000);
+            bebida.Beberse(500);
+            Console.WriteLine(bebida.Cantidad);
+
+            Cerveza cerveza = new Cerveza(500);
+            cerveza.Beberse(10);
         }
     }
 }
