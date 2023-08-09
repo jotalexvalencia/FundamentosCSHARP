a3d7307806f4fffc82602219f02f7d9e316856ea
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-08-09 17:52:56 -0500

    Program.cs: solicitudes POST, PUT, DELETE

--- FundamentosCSHARP/Program.cs
+++ FundamentosCSHARP/Program.cs
@@ -129,20 +129,98 @@ namespace FundamentosCSHARP
             //Cerveza cerveza = JsonSerializer.Deserialize<Cerveza>(miJson);
 
             #region 08 Solicitudes a servicios web por HTTP GET
-                string url = "https://jsonplaceholder.typicode.com/posts";
-                HttpClient client = new HttpClient();
-                
-                var httpResponse = await client.GetAsync(url);
+            //    string url = "https://jsonplaceholder.typicode.com/posts";
+            //    HttpClient client = new HttpClient();
 
-                if(httpResponse.IsSuccessStatusCode) 
-                { 
-                    var content = await httpResponse.Content.ReadAsStringAsync();
-                    List<Models.Post> post = 
-                    JsonSerializer.Deserialize<List<Models.Post>>(content);
-                }
+            //    var httpResponse = await client.GetAsync(url);
+
+            //    if(httpResponse.IsSuccessStatusCode) 
+            //    { 
+            //        var content = await httpResponse.Content.ReadAsStringAsync();
+            //        List<Models.Post> post = 
+            //        JsonSerializer.Deserialize<List<Models.Post>>(content);
+            //    }
 
 
 
+            #endregion
+
+            #region 09 Envío de información a Servicios Web por HTTP POST, PUT y DELETE 
+
+            // POST
+
+            //string url = "https://jsonplaceholder.typicode.com/posts";
+            //var client = new HttpClient();
+
+            //Post post = new Post()
+            //{
+            //    userId = 50,
+            //    body = "Hola como estan",
+            //    title = "titulo de saludo"
+            //};
+
+            //var data = JsonSerializer.Serialize<Post>(post);
+            //HttpContent content = 
+            //    new StringContent(data, System.Text.Encoding.UTF8,"application/json");
+
+            //var httpResponse = await client.PostAsync(url, content);
+
+            //if (httpResponse.IsSuccessStatusCode)
+            //{
+            //    var result = await httpResponse.Content.ReadAsStringAsync();
+
+            //    var postResult = JsonSerializer.Deserialize<Post>(result);
+            //}
+
+            // PUT
+
+            //string url = "https://jsonplaceholder.typicode.com/posts/99";
+            //var client = new HttpClient();
+
+            //Post post = new Post()
+            //{
+            //    userId = 50,
+            //    body = "Hola como estan",
+            //    title = "titulo de saludo"
+            //};
+
+            //var data = JsonSerializer.Serialize<Post>(post);
+            //HttpContent content =
+            //    new StringContent(data, System.Text.Encoding.UTF8, "application/json");
+
+            //var httpResponse = await client.PutAsync(url, content);
+
+            //if (httpResponse.IsSuccessStatusCode)
+            //{
+            //    var result = await httpResponse.Content.ReadAsStringAsync();
+
+            //    var postResult = JsonSerializer.Deserialize<Post>(result);
+            //}
+
+            // DELETE
+
+            string url = "https://jsonplaceholder.typicode.com/posts/99";
+            var client = new HttpClient();
+
+            Post post = new Post()
+            {
+                userId = 50,
+                body = "Hola como estan",
+                title = "titulo de saludo"
+            };
+
+            //var data = JsonSerializer.Serialize<Post>(post);
+            //HttpContent content =
+            //    new StringContent(data, System.Text.Encoding.UTF8, "application/json");
+
+            var httpResponse = await client.DeleteAsync(url);
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var result = await httpResponse.Content.ReadAsStringAsync();
+
+               // var postResult = JsonSerializer.Deserialize<Post>(result);
+            }
             #endregion
 
         }
