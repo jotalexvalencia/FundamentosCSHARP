bdb582727b616f589ac4206970f53dddaeaef6f0
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-11-10 15:06:48 -0500

    3 Uso de Arreglos y Listas

---
+++ 3_ArreglosListas/Models/Cerveza.cs
@@ -0,0 +1,15 @@
+﻿namespace _3_ArreglosListas.Models
+{
+    class Cerveza : Bebida
+    {
+        //public Cerveza() : base("Cerveza",500)
+        //{
+
+        //}
+
+        public Cerveza(int Cantidad, string Nombre = "Cerveza") : base(Nombre, Cantidad)
+        {
+
+        }
+    }
+}
---
+++ 3_ArreglosListas/Program.cs
@@ -0,0 +1,44 @@
+﻿using _3_ArreglosListas.Models;
+
+namespace _3_ArreglosListas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("iteración: " + i + " - " + numeros[i]);
+            }
+
+            foreach (var numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.WriteLine("********************************************");
+            List<int> lista = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+            //Queue<int> queue = new Queue<int>();// Colas
+            //Stack<int> stack = new Stack<int>();//Pilas
+            lista.Add(9);
+            lista.Add(0);
+            lista.Remove(2);
+
+            foreach (var numero in lista)
+            {
+                Console.WriteLine("elemento: " + numero);
+            }
+
+            List<Cerveza> cervezas = new List<Cerveza>() { new Cerveza(10, "Cerveza premium") };
+            cervezas.Add(new Cerveza(500));
+            Cerveza erdinger = new Cerveza(500, "Cerveza de trigo");
+            cervezas.Add(erdinger);
+
+            foreach (var cerveza in cervezas)
+            {
+                Console.WriteLine("cerveza: " + cerveza.Nombre);
+            }
+        }
+    }
+}
